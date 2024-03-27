b6ccb573dbb7475724f12498b06da3dd4c7072b6
zkhiraoui <za.khiraoui@gmail.com>
2024-03-27 05:52:17 -0700

    zakaria devops

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,48 +1,6 @@
-# Define class for Nginx installation and configuration
-class nginx {
-    # Install Nginx package
-    package { 'nginx':
-        ensure => installed,
-    }
+# Puppet Manifest to Install and Configure an Nginx Server
 
-    # Define Nginx service
-    service { 'nginx':
-        ensure  => running,
-        enable  => true,
-        require => Package['nginx'],
-    }
-
-    # Configure Nginx to listen on port 80
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => template('nginx/default.erb'),
-        notify  => Service['nginx'],
-    }
-}
-
-# Define content for the Nginx default configuration file
-file { '/etc/nginx/sites-available/default':
-    content => "server {
-        listen 80;
-        root /var/www/html;
-        index index.html index.htm;
-        
-        location / {
-            return 301 /redirect_me;
-        }
-        
-        location /redirect_me {
-            return 301 https://www.example.com;
-        }
-
-        location /hello {
-            return 200 'Hello, World!';
-        }
-    }",
-    require => Class['nginx'],
-}
-
-# Notify Nginx service to reload configuration after changes
-service { 'nginx':
-    subscribe => File['/etc/nginx/sites-available/default'],
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
